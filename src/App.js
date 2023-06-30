48c4119e13d9efcfec2dbe0ef32391f049c171ed
Aleksandr Savchenko <titamoto@gmail.com>
2023-06-30 12:36:21 -0700

    remove Switch from App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import FoodPage from "./components/FoodPage";
